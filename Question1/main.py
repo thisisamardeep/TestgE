cf820edcf79de17eaf221becf0a3742af94c8683
Amar <amardeepjk@gmail.com>
2020-12-02 08:58:50 +0800

    wip

---
+++ Question1/main.py
@@ -0,0 +1,17 @@
+def shuffle(x):
+    if x < 100:
+        return x
+    t = x
+    l = 0
+    while t > 0:
+        t //= 10
+        l += 1
+    a = x // 10 ** (l - 1) * 10 ** (l - 1)
+    b = (x % 10) * 10 ** (l - 2)
+    return a + b + shuffle((x - a) // 10)
+
+
+print(shuffle(123456))
+print(shuffle(5444574147))
+print(shuffle(3257423543222))
+
